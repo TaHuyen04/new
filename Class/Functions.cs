784a8e56768a6769ef4dc36c8946c9786b800854
TaHuyen04 <tahuyen1802@gmail.com>
2025-04-30 19:45:28 +0700

    a

---
+++ Class/Functions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menu.Class
+{
+    internal class Functions
+    {
+        public static DataTable getdatatotable(string sql)
+        {
+            SqlDataAdapter mydata = new SqlDataAdapter();
+            mydata.SelectCommand = new SqlCommand();
+            mydata.SelectCommand.Connection = Class.KetNoi.Conn;
+            mydata.SelectCommand.CommandText = sql;
+            DataTable table = new DataTable();
+            mydata.Fill(table);
+            return table;
+        }
+        public static bool IsDate(string d)
+        {
+            string[] parts = d.Split('/');
+            if ((Convert.ToInt32(parts[0]) >= 1) && (Convert.ToInt32(parts[0]) <= 31) &&
+(Convert.ToInt32(parts[1]) >= 1) && (Convert.ToInt32(parts[1]) <= 12) && (Convert.ToInt32(parts[2]) >= 1900))
+                return true;
+            else
+                return false;
+        }
+        public static string ConvertDateTime(string d)
+        {
+            string[] parts = d.Split('/');
+            string dt = String.Format("{0}/{1}/{2}", parts[1], parts[0], parts[2]);
+            return dt;
+        }
+        public static bool Checkkey(string sql)
+        {
+            SqlDataAdapter Mydata = new SqlDataAdapter(sql, KetNoi.Conn);
+            DataTable table = new DataTable();
+            Mydata.Fill(table);
+            if (table.Rows.Count > 0)
+                return true;
+            else return false;
+        }
+
+        public static void Runsql(string sql)
+        {
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = Class.KetNoi.Conn;
+            cmd.CommandText = sql;
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            cmd.Dispose();
+            cmd = null;
+        }
+        public static void Runsqldel(string sql)
+        {
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = Class.KetNoi.Conn;
+            cmd.CommandText = sql;
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Du lieu dang dung, ko the xoa dc", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            cmd.Dispose();
+            cmd = null;
+        }
+        public static void FillCombo(string sql, ComboBox cbo, string ma, string ten)
+        {
+            SqlDataAdapter Mydata = new SqlDataAdapter(sql, KetNoi.Conn);
+            DataTable table = new DataTable();
+            Mydata.Fill(table);
+            cbo.DataSource = table;
+
+            cbo.ValueMember = ma;    // Truong gia tri
+            cbo.DisplayMember = ten;    // Truong hien thi
+        }
+        public static string GetFieldValues(string sql)
+        {
+            string ma = "";
+            SqlCommand cmd = new SqlCommand(sql, KetNoi.Conn);
+            SqlDataReader reader;
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                ma = reader.GetValue(0).ToString();
+            }
+            reader.Close();
+            return ma;
+        }
+    }
+}
